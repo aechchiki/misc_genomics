0faf46f4aeeb2108054fc179c85fcd650569be33
Amina Echchiki <amina.echchiki@unil.ch>
2016-08-23 01:53:44 +0200

    get read name and length from fasta file

---
+++ FastaStats.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Suggested fasta files in current working directory: "
+ls *.fa*
+echo "---"
+
+# enter your file
+echo "Input the fasta file of your choice (.fasta or .fa), followed by [ENTER]:"
+read fastafile
+echo "---"
+
+# print read length
+echo "Getting reads name..."
+cat $fastafile | grep '^>' | cut -d " " -f1 | sed 's/>//' > $fastafile'_ReadName'
+echo "Getting reads length..."
+cat $fastafile | grep -v '^>' | awk '{ print length($0); }' > $fastafile'_ReadLength'
+echo "Printing reads name and length..."
+paste $fastafile'_ReadName' $fastafile'_ReadLength' > $fastafile'_ReadNameLength'
+echo "Done. Output written to $fastafile'_ReadNameLength'"
+echo "---"
