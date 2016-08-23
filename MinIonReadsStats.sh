8f07d47a2667df550ee154c31d8773336f8d931d
Amina Echchiki <amina.echchiki@unil.ch>
2016-08-23 11:05:33 +0200

    MinION read type and proportions, files in LGTF format

---
+++ MinIonReadsStats.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "Suggested fastq files in current working directory: "
+ls *.fastq
+echo "---"
+
+# enter your file
+echo "Input the fastq file of your choice (.fasta or .fa), followed by [ENTER]:"
+read fastqfile
+echo "---"
+
+# print read length
+echo "Total read count: "
+tot=$(cat $fastqfile | grep ^@ | wc -l )
+echo "$tot"
+
+echo "2D reads count: " 
+twod=$(cat $fastqfile | grep ^@ | grep '_2d' | wc -l)
+echo "$twod"
+echo "Proportion of 2D reads:" ; bc -l <<< $twod/$tot
+
+echo "Template read count: "
+template=$(cat $fastqfile | grep ^@ | grep '_template' | wc -l)
+echo "$template"
+echo "Proportion of template reads:" ; bc -l <<<  $template/$tot
+
+echo "Complement read count: "
+complement=$(cat $fastqfile | grep ^@ | grep '_complement' | wc -l)
+echo "$complement"
+echo "Proportion of complement reads:"; bc -l <<< $complement/$tot
